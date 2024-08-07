62182021b434b6e68db1640c6204f5a6f0d12e5f
gosleii <sleii@inbox.ru>
2024-08-07 19:39:25 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import telebot
+
+bot = telebot.TeleBot("7242541800:AAG4e7pfIO7YI2Qi3nCvj9Q6di97m7SHt9E")
+
+
+@bot.message_handler(commands=["lets_start"])
+def start_handler(message):
+    bot.send_message(message.chat.id, "hello world")
+
+
+@bot.message_handler(commands=["newcommand"])
+def start_handler(message):
+    bot.send_message(message.chat.id, "[I want sleep](https://t.me/urikalegenda)", parse_mode="Markdown")
+
+
+bot.infinity_polling()
